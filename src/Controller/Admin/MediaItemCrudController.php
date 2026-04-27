df59747e6eb5f702165f9c2fb1e7561b7264d201
Lord of Chicken <gael.layeux@poulet.org>
2026-04-27 22:04:24 +0200

    Remove unused reorder route and references to fix RouteNotFoundException

--- src/Controller/Admin/MediaItemCrudController.php
+++ src/Controller/Admin/MediaItemCrudController.php
@@ -13,11 +13,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 
 class MediaItemCrudController extends AbstractCrudController
 {
@@ -73,33 +68,5 @@ class MediaItemCrudController extends AbstractCrudController
         return $actions
             ->add(Crud::PAGE_INDEX, $editImageAction)
             ->add(Crud::PAGE_EDIT, $editImageAction);
-    }
-
-    #[Route('/admin/media-item/reorder', name: 'app_admin_media_item_reorder', methods: ['POST'])]
-    public function reorder(
-        Request $request,
-        MediaItemRepository $mediaItemRepository,
-        EntityManagerInterface $entityManager
-    ): JsonResponse {
-        $data = json_decode($request->getContent(), true);
-
-        if (!isset($data['orders']) || !is_array($data['orders'])) {
-            return new JsonResponse(['success' => false, 'error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
-        }
-
-        foreach ($data['orders'] as $orderData) {
-            if (!isset($orderData['id']) || !isset($orderData['sortOrder'])) {
-                continue;
-            }
-
-            $mediaItem = $mediaItemRepository->find($orderData['id']);
-            if ($mediaItem) {
-                $mediaItem->setSortOrder($orderData['sortOrder']);
-            }
-        }
-
-        $entityManager->flush();
-
-        return new JsonResponse(['success' => true]);
     }
 }
