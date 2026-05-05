95f45e7bfe53b08e5f4858302e28a4a3d1c41720
Lord of Chicken <gael.layeux@poulet.org>
2026-05-05 12:13:03 +0200

    Fix: add page variables to FAQ controller
    - Add TranslatorInterface dependency to FaqController
    - Pass page title and intro variables to template
    - Fix RuntimeError for undefined page variable

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class FaqController extends AbstractController
 {
     #[Route('/faq', name: 'app_faq')]
-    public function index(): Response
+    public function index(TranslatorInterface $translator): Response
     {
-        return $this->render('faq/index.html.twig');
+        return $this->render('faq/index.html.twig', [
+            'page' => [
+                'title' => $translator->trans('faq.title'),
+                'intro' => $translator->trans('faq.meta_description'),
+            ],
+        ]);
     }
 }
