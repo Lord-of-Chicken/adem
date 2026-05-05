a0124ffcc8bfb1b363ae4def2864a4346121bbe3
Lord of Chicken <gael.layeux@poulet.org>
2026-05-05 12:10:32 +0200

    Fix: simplify FAQ route to match application pattern
    - Remove {_locale} parameter from FAQ route
    - Use simple /faq path which will be localized by Symfony's locale system
    - This matches the pattern used by ContactController
    - Fix RouteNotFoundException error

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -4,11 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class FaqController extends AbstractController
 {
-    #[Route('/{_locale}/faq', name: 'app_faq', requirements: ['_locale' => 'fr|en'])]
+    #[Route('/faq', name: 'app_faq')]
     public function index(): Response
     {
         return $this->render('faq/index.html.twig');
