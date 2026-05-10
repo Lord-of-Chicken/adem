55331e3b0925953dcffd0774644bffcf8a3ee26a
Lord of Chicken <gael.layeux@poulet.org>
2026-05-10 14:10:40 +0200

    Fix MySQL version and migration for Docker database

--- migrations/Version20260504074908.php
+++ migrations/Version20260504074908.php
@@ -22,7 +22,7 @@ final class Version20260504074908 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE nom_entite (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE participation_tier (id VARCHAR(32) NOT NULL, title VARCHAR(255) NOT NULL, detail LONGTEXT NOT NULL, price_label VARCHAR(16) NOT NULL, price_unit VARCHAR(8) NOT NULL, price_suffix VARCHAR(32) DEFAULT NULL, unit_price_eur NUMERIC(10, 2) NOT NULL, priced_per_unit TINYINT NOT NULL, min_qty INT NOT NULL, max_qty INT NOT NULL, tier_group VARCHAR(16) NOT NULL, donor_field TINYINT NOT NULL, sort_order INT NOT NULL, active TINYINT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('ALTER TABLE `order` DROP currency');
+        // Skip DROP currency as it doesn't exist in fresh database
     }
 
     public function down(Schema $schema): void
